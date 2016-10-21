e4333b4e15b014eeb4b21caccce19c26f3c3a0fc
Chris <chris.kolann@gmail.com>
2016-10-21 09:56:29 +0200

    Initial release with some changes to the original

---
+++ ExtendedOperations/Mul.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+
+namespace ExtendedOperations
+{
+    [Export(typeof(SimpleCalculator3.IOperation))]
+    [ExportMetadata("Symbol", '*')]
+	public class Mul : SimpleCalculator3.IOperation
+	{
+	    public int Operate(int left, int right)
+	    {
+		    return left * right;
+	    }
+	}
+}
